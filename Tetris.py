0ac3c88cb4a04609b187283d8850224e1112f80e
sharma2007 <92726699+sharma2007@users.noreply.github.com>
2021-10-19 12:48:32 +0400

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -1,8 +1,9 @@
 import pygame, sys
 import random
 
+cell_size = 18
 cols = 10
-rows = 40
+rows = 20
 maxfps = 30
 
 colours = [
@@ -37,3 +38,28 @@ shapes = [
      [['T', 'T', 'T'],
      [0, 'T', 0]],
 ]]
+
+class Tetris(object):
+      def __init__(self):
+        pygame.init()
+        pygame.key.set_repeat(250, 25)
+        self.width = cell_size * (cols + 6)
+        self.height = cell_size * rows
+        self.rlim = cell_size * cols
+        self.bground_grid = [[ 0 for x in range(cols)] for y in range(rows)]
+
+        self.default_font = pygame.font.Font(
+            pygame.font.get_default_font(), 12)
+
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.event.set_blocked(pygame.MOUSEMOTION)  # We do not need
+        # mouse movement
+        # events, so we
+        # block them.
+        self.next_stone = shapes[rand(len(shapes))]
+        self.init_game()
+        
+def rotate_clockwise(shape):
+    return [[shape[y][x]
+             for y in range(len(shape))]
+            for x in range(len(shape[0]) - 1, -1, -1)]
