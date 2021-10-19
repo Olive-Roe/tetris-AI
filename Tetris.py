947afea82e2031f0e840fb4890d941ba85e9c076
Olive-Roe <14oloh@kl.his.edu.my>
2021-10-19 17:18:56 +0800

    bugfix

--- Tetris.py
+++ Tetris.py
@@ -167,7 +167,7 @@ def create_grid(locked_positions={}):
       grid = [[(0,0,0) for x in range(10)] for x in range(20)]
       
       for i in range(len(grid)):
-          for j in range(len(grid[1]))
+          for j in range(len(grid[1])):
               if (j,i) in locked_positions:
                   c = locked_positions[(j,i)]
                   grid[i][j] = c
